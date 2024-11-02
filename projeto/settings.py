cc9c00eb4df0b200935636288da036e0fb4e70b9
Kauanrodrigues01 <kauanrl09@gmail.com>
2024-11-02 15:58:42 -0300

    configurando url das imagens

--- projeto/settings.py
+++ projeto/settings.py
@@ -123,10 +123,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "base_static",
+    BASE_DIR / 'base_static',
 ]
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
