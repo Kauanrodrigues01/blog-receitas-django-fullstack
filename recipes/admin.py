7a982698c50e317fca80c5b50895e7d8da8bb12a
Kauanrodrigues01 <kauanrl09@gmail.com>
2024-11-02 21:36:00 -0300

    feat: adicionando filtro por categoria

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Category, Recipe
 
 class CategoryAdmin(admin.ModelAdmin):
-    ...
-    
-class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'name')  # Exibe o ID e o nome da categoria na lista
+    search_fields = ('name',)  # Permite buscar categorias pelo nome
+    ordering = ('name',)  # Ordena a lista pelo nome da categoria
 
-admin.site.register(Recipe, RecipeAdmin)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'author', 'is_published', 'created_at')  # Campos a serem exibidos na lista
+    search_fields = ('title', 'description')  # Permite buscar receitas pelo título ou descrição
+    list_filter = ('category', 'is_published')  # Adiciona filtros para a lista
+    prepopulated_fields = {'slug': ('title',)}  # Preenche o campo slug com base no título
+    ordering = ('-created_at',)  # Ordena a lista pela data de criação (mais recente primeiro)
+
+# Registro dos modelos com suas classes admin
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe, Category
 
 def home(request):
-    contexto = {
-        'recipes': [make_recipe() for _ in range(10)]
+    context = {
+        'recipes': Recipe.objects.filter(is_published=True).order_by('-id')
     }
-    return render(request, 'recipes/pages/home.html', context=contexto)
+    return render(request, 'recipes/pages/home.html', context)
 
 
 def recipe(request, id):
-    contexto = {
-        'recipe': make_recipe(),
+    context = {
+        'recipe': Recipe.objects.filter(id=id, is_published=True).first(),
         'is_detail_page': True
     }
-    return render(request, 'recipes/pages/recipe-view.html', context=contexto)
+    
+    return render(request, 'recipes/pages/recipe-view.html', context)
+
+def recipes_by_category(request, category_id):
+    category = None
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        pass
+    
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    
+    context = {
+        'recipes': recipes,
+        'category_exists': True if category else False,
+        'category_name': category.name if category else 'Category does not exist'
+    }
+    
+    return render(request, 'recipes/pages/category.html', context)
+
